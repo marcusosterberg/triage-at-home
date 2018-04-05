2841ad5d4898443cea2938931ad5bcc954f0e01f
Marcus Österberg <marcus@webbfunktion.com>
2018-04-05 17:43:11 +0200

    Less bugs :)

--- poc-computer-vision.py
+++ poc-computer-vision.py
@@ -7,6 +7,7 @@ vision_analyze_url = vision_base_url + "analyze"
 import requests
 
 def post_image(image_url):
+	print('Posting URL to Azure: ', image_url)
 	headers  = {'Ocp-Apim-Subscription-Key': subscription_key }
 	params   = {'visualFeatures': 'Categories,Description,Color'}
 	data     = {'url': image_url}
@@ -16,9 +17,9 @@ def post_image(image_url):
 
 	image_caption = analysis["description"]["captions"][0]["text"].capitalize()
 	image_caption
-	print(image_url, image_caption)
+	print(image_url, "\n", image_caption)
 
-import requests
+import requests, sys
 
 def httpRequestGetContent(url):
     """Trying to fetch the response content
@@ -45,6 +46,8 @@ def httpRequestGetContent(url):
         pass
 
 from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+
 rss_feed_url = "http://www.vgregion.se/Public/RssResult/FeedCombined/149801?name=Aktuellt"
 rss_feed = httpRequestGetContent(rss_feed_url)
 soup = BeautifulSoup(rss_feed, "html.parser")
@@ -53,7 +56,12 @@ soup = BeautifulSoup(rss_feed, "html.parser")
 ### Fetching URL:s from RSS feed
 i = 1
 for items in soup.find_all("item"):
-	url = items.contents[1]
+	url = ""
+	if "http" in items.contents[1]:
+		url = items.contents[1]
+	elif "http" in items.contents[2]:
+		url = items.contents[2]
+
 	print("Found an URL in the feed:\n{0}. {1}".format(i, url))
 	i += 1
 	
@@ -63,8 +71,13 @@ for items in soup.find_all("item"):
 	for image in another_soup.find_all('img', src=True):
 		if '.jpg' in image['src']:
 			found_image += 1
-			print('{0}. {1}'.format(found_image, image['src']))
+			#print('{0}. {1}'.format(found_image, image['src']))
 
 			### sends the first JPG to Azure API for computer vision
-			post_image('{0}{1}{2}'.format('http://', rss_feed_url.split('/')[2], image['src']))
+			if "http" in image["src"]:
+				### scheme is explicitly stated in image URL
+				post_image(image['src'])
+			else:
+				### making image URL based on feed address
+				post_image('{0}://{1}/{2}'.format(urlparse(rss_feed_url).scheme, urlparse(rss_feed_url).netloc, image['src']))
 			break
